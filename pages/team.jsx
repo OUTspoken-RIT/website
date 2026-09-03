9042359a6db8b312da8e602e34dba3b8feac0d77
Emily <63062480+emmi412@users.noreply.github.com>
2026-09-02 21:12:43 -0400

    Revise officer information on team page
    Updated officer details including names, positions, pronouns, flags, emails, office hours, majors, and fun facts. Put in placeholder fields for positions we're actively hiring for. Have not changed photos but need to.

--- pages/team.jsx
+++ pages/team.jsx
@@ -31,17 +31,16 @@ export default function Team() {
       <CenteredLayout color="foam">
         <OfficerBio
           image="https://campusgroups.rit.edu/upload/rit/2025/s3_image_5160001_Angelis_5462edc9-46c5-4ea9-a0a2-05a1244572d2_112204328.jpg"
-          name="Angelis Cordero"
+          name="Emily Lu"
           position="President"
           pronouns="She/Her"
-          flags="lesbian"
-          email="ajc4439@g.rit.edu"
-          officeHours="M: 12-1pm, T: 1-2pm, & W: 4:30-6:30pm"
+          flags="intersexinclusive"
+          email="el2902@rit.edu"
+          officeHours="Friday 10am-2pm"
           // officeHoursUrl="https://rit.zoom.us/j/95076270332"
         >
-          <OfficerMajor>5th Year - Robotics and Manufacturing Engineering Technology BS</OfficerMajor>
-          <OfficerCoffeeOrder>My go-to coffee order!
-            Iced coffee from Dunkin with two pumps of butter pecan and 1 caramel pump with coffee oat milk</OfficerCoffeeOrder>
+          <OfficerMajor>2nd Year - Graphic Design BFA</OfficerMajor>
+          <OfficerFunFact>My favorite movie! The Half of It</OfficerFunFact>
           {/* <OfficerWorkingOn>I'm excited to work on ...</OfficerWorkingOn> */}
         </OfficerBio>
         <OfficerBio
@@ -50,53 +49,51 @@ export default function Team() {
           position="Vice President"
           pronouns="He/They"
           flags="trans nb pan"
-          email="ac1822@g.rit.edu"
-          officeHours="T & Th: 12:30-1:30pm, W: 4:30-6:30pm"
+          email="ac1822@rit.edu"
+          officeHours="Wed: 3-5pm, Thurs: 12-2pm"
         >
-          <OfficerMajor>2nd Year - Electrical Engineering BS</OfficerMajor>
+          <OfficerMajor>3rd Year - Electrical Engineering BS</OfficerMajor>
           <OfficerFunFact>My current hobby! 
             Crochet and Lobotomy Corporation</OfficerFunFact>
           {/* <OfficerWorkingOn>I'm excited to work on ...</OfficerWorkingOn> */}
         </OfficerBio>
         <OfficerBio
           image="https://campusgroups.rit.edu/upload/rit/2026/s3_image_5160001_lu_57cecbd6-fc8c-4bf4-9753-7c25c4f832e7_3316227.jpg"
-          name="Emily Lu"
+          name="We Are Hiring a Director of Finance for the 26-27 Academic Year!"
           position="Director of Finance"
-          pronouns="She/They"
+          pronouns="Pro/Nouns"
           flags="intersexinclusive"
-          email="el2902@rit.edu"
-          officeHours="F: 10-11am, 12-2pm, & 3-4pm"
+          email="abc1234@rit.edu"
+          officeHours="n/a"
         >
-          <OfficerMajor>2nd Year - Graphic Design BA</OfficerMajor>
-          <OfficerFunFact>My favorite movie! The Half of It</OfficerFunFact>
+          <OfficerMajor>Year & Major</OfficerMajor>
+          <OfficerFunFact> </OfficerFunFact>
           {/* <OfficerWorkingOn></OfficerWorkingOn> */}
         </OfficerBio>
         <OfficerBio
           image="https://campusgroups.rit.edu/upload/rit/2025/s3_image_5160001_Elena_8f5d65d8-9559-4f1b-b9b8-f874ded7b38f_11220441.jpg"
-          name="Elena Georgia Mpadanes"
-          position="Director of Programming"
-          pronouns="She/Her"
-          flags="bi intersexinclusive"
-          email="em1792@rit.edu"
-          officeHours="T & Th: 11:15am-12:15pm"
+          name="We Are Hiring Directors of Events for the 26-27 Academic Year!"
+          position="Director of Events"
+          pronouns="Pro/Nouns"
+          flags="intersexinclusive"
+          email="abc1234@rit.edu"
+          officeHours="n/a"
         >
-          <OfficerMajor>2nd Year - Cognitive Science PhD</OfficerMajor>
-          <OfficerFunFact>Fun fact about me!
-            I competed in the pickle juice drinking contest at Picklesburgh 2025 (didn't win though..) </OfficerFunFact>
+          <OfficerMajor>Year & Major</OfficerMajor>
+          <OfficerFunFact>This could be a fun fact about you ;)</OfficerFunFact>
           {/* <OfficerWorkingOn>I'm excited to work on ...</OfficerWorkingOn> */}
         </OfficerBio>
         <OfficerBio
           image="https://campusgroups.rit.edu/upload/rit/2025/s3_image_5160001_JD_27529fc9-9bde-41d1-a0ce-2a1eb02720a6_112204417.jpg"
-          name="JD Berzolla"
+          name="We Are Hiring a Director of Marketing for the 26-27 Academic Year!"
           position="Director of Marketing"
-          pronouns="He/They"
-          flags="demiboy demiromantic intersexinclusive"
-          email="jdb8337@rit.edu"
-          officeHours="T: 3:30-5:30pm"
+          pronouns="Pro/Nouns"
+          flags="intersexinclusive"
+          email="n/a"
+          officeHours="n/a"
         >
-          <OfficerMajor>1st Year - B.S. Software Engineering</OfficerMajor>
-          <OfficerCoffeeOrder>My go-to coffee order!
-            Iced Carmel Macchiato</OfficerCoffeeOrder>
+          <OfficerMajor>Year & Major</OfficerMajor>
+          <OfficerCoffeeOrder>n/a</OfficerCoffeeOrder>
           {/* <OfficerWorkingOn>I'm excited to work on ...</OfficerWorkingOn> */}
         </OfficerBio>
         <OfficerBio
@@ -106,9 +103,9 @@ export default function Team() {
           pronouns="They/Them"
           flags="bi unlabled poly"
           email="bjh3059@g.rit.edu"
-          officeHours="F: 2-4pm"
+          officeHours="Wed: 5-7pm"
         >
-          <OfficerMajor>3rd Year - Medical Illustration BFA</OfficerMajor>
+          <OfficerMajor>4th Year - Medical Illustration BFA</OfficerMajor>
           <OfficerCoffeeOrder>My go-to coffee order!
             Iced chai with oat milk</OfficerCoffeeOrder>
           {/* <OfficerWorkingOn>I'm excited to work on ...</OfficerWorkingOn> */}
